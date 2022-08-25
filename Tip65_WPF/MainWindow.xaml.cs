914a82d9fcf54c97a9479bede44c862236d78955
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-08-25 18:58:20 +0800

    测试多个窗口捕获非捕获的异常处理
    测试多个窗口捕获非捕获的异常处理, 但是结果很可惜, 一个wpf应用, 异常是同享的

---
+++ Tip65_WPF/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Tip65_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Dispatcher.UnhandledException += new DispatcherUnhandledExceptionEventHandler(Application_DispatcherUnhandledException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = e.ExceptionObject as Exception;
+                string errorMsg = "非WPF窗体线程异常 : \n\n";
+                MessageBox.Show(errorMsg + ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+            catch
+            {
+                try
+                {
+                    MessageBox.Show("不可恢复的WPF窗体线程异常，应用程序将退出！");
+                }
+                finally
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = e.Exception;
+                string errorMsg = "WPF窗体线程异常 : \n\n";
+                MessageBox.Show(errorMsg + ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+            catch
+            {
+                try
+                {
+                    MessageBox.Show("不可恢复的WPF窗体线程异常，应用程序将退出！");
+                }
+                finally
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+            e.Handled = true;
+        }
+
+        private void btn_throw_Click(object sender, RoutedEventArgs e)
+        {
+            throw new InvalidOperationException("主界面抛出了异常");
+        }
+
+        private void btn_ShowOther_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+    }
+}
---
+++ Tip65_WPF/Window1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Tip65_WPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            this.Dispatcher.UnhandledException += new DispatcherUnhandledExceptionEventHandler(Application_DispatcherUnhandledException);
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = e.Exception;
+                string errorMsg = "WPF窗体线程异常 : \n\n";
+                MessageBox.Show(errorMsg + ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+            catch
+            {
+                try
+                {
+                    MessageBox.Show("不可恢复的WPF窗体线程异常，应用程序将退出！");
+                }
+                finally
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+            e.Handled = true;
+        }
+
+        private void btn_throw_Click(object sender, RoutedEventArgs e)
+        {
+            throw new InvalidOperationException("主界面抛出了异常");
+        }
+    }
+}
